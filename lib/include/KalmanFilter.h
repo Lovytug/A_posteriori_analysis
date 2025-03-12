8df07bf83c738abb71fbe1dfc02b0499b0718d68
Danya <8751290@bk.ru>
2025-03-12 16:54:43 +0300

    полная реализация фильтра калмена

--- lib/include/KalmanFilter.h
+++ lib/include/KalmanFilter.h
@@ -9,14 +9,23 @@ namespace apa
 	class KalmanFilter
 	{
 	public:
-		KalmanFilter();
-		void porfomFiltring(const Vector& vec);
+		KalmanFilter(const Vector& vec, const Matrix& matrix);
+		void perfomFiltring(const Vector& vecMeasurement, const Vector& vecVelocityAiminig);
 		Vector getVectorDelta_awesomeState();
+		Matrix getMatrixDelta_awesomeMistakeState();
+
 
 	private:
-		Vector vectorDelta_awesomeState;
+		Vector vectorDelta_awesomeState; // X*
+		Matrix matrixDelta_awesomeMistakeState; // P*
+		Vector vectorDelta_forecastState; // X^
+		Matrix matrixDelta_forecastMistakeState; // P^
 		Matrix H;
 		Matrix F;
-		Matrix P;
+		Matrix L;
+		Matrix U;
+		Matrix V;
+		Matrix D_nu;
+		Matrix D_ksi;
 	};
 }
---
+++ lib/src/KalmanFilter.cpp
@@ -0,0 +1,83 @@
+#include "KalmanFilter.h"
+
+apa::KalmanFilter::KalmanFilter(const Vector& vec_forescast, const Matrix& matrix_forecast)
+{
+	int32_t n = 4;
+	int32_t l = 2;
+	int32_t k = 2;
+	int32_t m = 4;
+	vectorDelta_awesomeState.resize(n);
+	matrixDelta_awesomeMistakeState(n, n);
+	vectorDelta_forecastState.resize(n);
+	matrixDelta_forecastMistakeState.resize(n, n);
+	vectorDelta_forecastState = vec_forescast;
+	matrixDelta_forecastMistakeState = matrix_forecast;
+
+
+	F.resize(n, n);
+	F << 1, 0, 1, 0,
+		0, 1, 0, 1,
+		0, 0, 1, 0,
+		0, 0, 0, 1;
+
+	H.resize(l, n);
+	H << 1, 0, 0, 0,
+		0, 1, 0, 0;
+
+	D_nu.resize(l, l); // matrix cov locator
+	D_nu << 10, 0,
+			0, 10;
+
+	U.resize(k, 1); // dynamic add Vh ~ aimingVelisity
+	U.setZero();
+
+	V.resize(n, k);
+	V << 1, 0,
+		0, 1,
+		0, 0,
+		0, 0;
+
+	L.resize(n, m);
+	L << 0, 0, 0, 0,
+		0, 0, 0, 0,
+		1, 0, -1, 0,
+		0, 1, 0, -1;
+
+	D_ksi.resize(m, m); // matrix cov wind and wave
+	D_ksi << 0.25, 0, 0, 0,
+			0, 0.25, 0, 0,
+			0, 0, 1, 0,
+			0, 0, 0, 1;
+	
+}
+
+void apa::KalmanFilter::perfomFiltring(const Vector& vecMeas, const Vector& vecVelH)
+{
+	Matrix inverse_matrixForecast = matrixDelta_forecastMistakeState.inverse();
+	Matrix H_tr = H.transpose();
+	Matrix D_nu_in = D_nu.inverse();
+	matrixDelta_awesomeMistakeState = (inverse_matrixForecast + H_tr * D_nu_in * H).inverse();
+	vectorDelta_awesomeState = vectorDelta_forecastState + matrixDelta_awesomeMistakeState * H_tr * D_nu_in * (vecMeas - H * vectorDelta_forecastState);
+
+
+	Matrix next_matrixDelta_forecastMistState(4, 4);
+	Vector next_vectorDelta_forecastState(4);
+
+	U = vecVelH;
+	next_matrixDelta_forecastMistState = F * matrixDelta_awesomeMistakeState * F.transpose() + L * D_ksi * L.transpose();
+	next_vectorDelta_forecastState = F * vectorDelta_awesomeState * V * U;
+
+
+	matrixDelta_forecastMistakeState = next_matrixDelta_forecastMistState;
+	vectorDelta_forecastState = next_vectorDelta_forecastState;
+}
+
+Vector apa::KalmanFilter::getVectorDelta_awesomeState()
+{
+	return vectorDelta_awesomeState;
+}
+
+Matrix apa::KalmanFilter::getMatrixDelta_awesomeMistakeState()
+{
+	return matrixDelta_awesomeMistakeState;
+}
